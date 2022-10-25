b6b82e858474aa841b9cf43882e655a6ceece8ea
Sander van Driel <sander.vandriel@gmail.com>
2022-10-25 11:26:26 +0200

    refactor: exec-ssh-command

--- src/ssh-exec-cmd.spec.ts
+++ src/ssh-exec-cmd.spec.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from 'events';
 import { Client, ClientChannel } from 'ssh2';
 import * as stream from 'stream';
 import { sshExecCommand } from './ssh-exec-cmd';
+import { tick } from './util/tick';
 
 describe('sshExecCommand', () => {
     it('works', async () => {
@@ -15,6 +16,7 @@ describe('sshExecCommand', () => {
         } as unknown as Client;
         const resultPromise = sshExecCommand(client, 'ls', ['-la']);
 
+        await tick();
         stdout.emit('data', Buffer.from('Standard out', 'utf-8'));
         stdout.stderr.emit('data', Buffer.from('Standard error', 'utf-8'));
         stdout.emit('close', 0);
@@ -27,5 +29,45 @@ describe('sshExecCommand', () => {
             stdout: 'Standard out',
             stderr: 'Standard error'
         });
+    });
+
+    it('fails when command fails', async () => {
+        const stdout = new EventEmitter() as ClientChannel;
+        stdout.stderr = new EventEmitter() as stream.Readable;
+        const exec = jest.fn().mockImplementation((_commandLine, callback) => {
+            callback(new Error('fail'));
+        });
+        const client = {
+            exec
+        } as unknown as Client;
+
+        await expect(sshExecCommand(client, 'ls', ['-la'])).rejects.toEqual(new Error('fail'));
+    });
+
+    it('fails on non-zero exit code', async () => {
+        const stdout = new EventEmitter() as ClientChannel;
+        stdout.stderr = new EventEmitter() as stream.Readable;
+        const exec = jest.fn().mockImplementation((_commandLine, callback) => {
+            callback(null, stdout);
+        });
+        const client = {
+            exec
+        } as unknown as Client;
+        const resultPromise = sshExecCommand(client, 'ls', ['-la']);
+
+        await tick();
+        stdout.emit('data', Buffer.from('Standard out', 'utf-8'));
+        stdout.stderr.emit('data', Buffer.from('Standard error', 'utf-8'));
+        stdout.emit('close', -1);
+
+        const expected = new Error('Command returned non-zero error code: -1 (signal: undefined)');
+        Object.assign(expected, {
+            stdout: 'Standard out',
+            stderr: 'Standard error',
+            code: -1,
+            cmd: 'ls -la'
+        });
+
+        await expect(resultPromise).rejects.toThrow(expected);
     });
 });
--- src/ssh-exec-cmd.ts
+++ src/ssh-exec-cmd.ts
@@ -1,5 +1,11 @@
-import { Client } from 'ssh2';
+import { Client, ClientChannel } from 'ssh2';
+import { promisify } from 'util';
 import { bashEscape } from './util/bash-escape';
+
+export interface SshCommandResult {
+    stdout: string;
+    stderr: string;
+}
 
 /**
  * Executes a command on an (open) SSH2 client
@@ -7,45 +13,41 @@ import { bashEscape } from './util/bash-escape';
  * @param cmd Command to execute
  * @returns The stdout and stderr of the operation
  */
-export function sshExecCommand(
+export async function sshExecCommand(
     conn: Client,
     cmd: string,
     args: string[] = [],
     verbose = false
-): Promise<{
-    stdout: string;
-    stderr: string;
-    verbose?: boolean;
-}> {
+): Promise<SshCommandResult> {
     const commandLine = `${[cmd, ...args].map(bashEscape).join(' ')}`;
     if (verbose) {
         console.log(`Running ${commandLine}`);
     }
 
-    return new Promise((resolve, reject) => {
+    const exec = promisify(conn.exec.bind(conn));
+    const channel = await exec(commandLine);
+    return handleCommand(channel, commandLine);
+}
+
+function handleCommand(stream: ClientChannel, commandLine: string): Promise<SshCommandResult> {
+    return new Promise<SshCommandResult>((resolve, reject) => {
         let stdout = '';
         let stderr = '';
-        conn.exec(commandLine, (err, stream) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            stream
-                .on('close', (code: number, signal?: string) => {
-                    if (code === 0) {
-                        resolve({ stdout, stderr });
-                    } else {
-                        const error = new Error(`Command returned non-zero error code: ${code} (signal: ${signal})`);
-                        Object.assign(error, { stdout, stderr, code, signal, cmd: commandLine });
-                        reject(error);
-                    }
-                })
-                .on('data', (data: Buffer) => {
-                    stdout += data.toString();
-                })
-                .stderr.on('data', (data: Buffer) => {
-                    stderr += data.toString();
-                });
-        });
+        stream
+            .on('close', (code: number, signal?: string) => {
+                if (code === 0) {
+                    resolve({ stdout, stderr });
+                } else {
+                    const error = new Error(`Command returned non-zero error code: ${code} (signal: ${signal})`);
+                    Object.assign(error, { stdout, stderr, code, signal, cmd: commandLine });
+                    reject(error);
+                }
+            })
+            .on('data', (data: Buffer) => {
+                stdout += data.toString();
+            })
+            .stderr.on('data', (data: Buffer) => {
+                stderr += data.toString();
+            });
     });
 }
